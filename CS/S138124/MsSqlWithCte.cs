d2d73845349316e0414e14fd30a966a1449bb31d
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2025-02-14 20:08:25 +0400

    Update MsSqlWithCte.cs

--- CS/S138124/MsSqlWithCte.cs
+++ CS/S138124/MsSqlWithCte.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using Microsoft.Data.SqlClient;
 using DevExpress.Xpo;
 using DevExpress.Xpo.Helpers;
